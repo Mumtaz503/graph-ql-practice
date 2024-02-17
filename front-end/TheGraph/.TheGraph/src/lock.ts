ddb16e8997b0459d3f6aa3499691324782deba2f
Mumtaz503 <132309236+Mumtaz503@users.noreply.github.com>
2024-02-17 16:25:22 +0000

    2nd commit

--- front-end/TheGraph/.TheGraph/src/lock.ts
+++ front-end/TheGraph/.TheGraph/src/lock.ts
@@ -1,16 +1,28 @@
 import { InfoAdded as InfoAddedEvent } from "../generated/Lock/Lock"
 import { InfoAdded } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleInfoAdded(event: InfoAddedEvent): void {
-  let entity = new InfoAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.param0_age = event.params.param0.age
-  entity.param0_name = event.params.param0.name
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let infoAdded = InfoAdded.load(getIdFromEventParams(event.params._age, event.params._name))
 
-  entity.save()
+
+
+
+
+  // let entity = new InfoAdded(
+  //   event.transaction.hash.concatI32(event.logIndex.toI32())
+  // )
+  // entity.param0_age = event.params.param0.age
+  // entity.param0_name = event.params.param0.name
+
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
+
+  // entity.save()
+}
+
+function getIdFromEventParams(age: BigInt, name: string): string {
+  return age.toHexString() + name.toString()
 }
